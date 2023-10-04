ce7acc20c131d6aa170acd9c3cb2dbd9e0dc78d9
Tilacin <tilacin45@gmail.com>
2023-10-05 01:10:52 +0300

    настраивал изображения категорий: увелечение при наведении , смена фона, перестройка в одну колонну

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,26 +1,61 @@
-
 import Image from "next/image";
 import React from "react";
 
 function SingleProductPage() {
   return (
-    <div className="flex justify-around  items-center px-2">
-      <div className="flex flex-col">
-        Первые блюда
-        <Image src="/main_first.jpg" width={450} height={250} alt="cart"/>
+    <div className="p-2 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
+      <div className="flex flex-col m-2 hover:bg-orange-100  ">
+        <h1 className="uppercase font-bold text-2xl text-red-500 ">
+          Первое
+        </h1>
+        <div className="overflow-hidden">
+          <Image
+            className="hover:scale-110 transition duration-500 cursor-pointer object-cover "
+            src="/main_first.jpg"
+            width={450}
+            height={250}
+            alt="cart"
+          />
         </div>
-      <div>
-        Вторые блюда
-        <Image src="/main_second.jpg" width={450} height={250} alt="cart"/>
+      </div>
+      <div className="flex flex-col m-2 hover:bg-orange-100">
+        <h1 className="uppercase font-bold text-2xl text-red-500">
+          Второе
+        </h1>
+        <div className="overflow-hidden">
+          <Image
+            className="hover:scale-110 transition duration-500 cursor-pointer object-cover "
+            src="/main_second.jpg"
+            width={450}
+            height={250}
+            alt="cart"
+          />
         </div>
-      <div>
-        Напитки
-        <Image src="/main_drinks.jpg"  width={450} height={250} alt="cart"/>
+      </div>
+      <div className="flex flex-col m-2 hover:bg-orange-100">
+        <h1 className="uppercase font-bold text-2xl text-red-500">Напитки</h1>
+        <div className="overflow-hidden">
+          <Image
+            className="hover:scale-110 transition duration-500 cursor-pointer object-cover "
+            src="/main_drinks.jpg"
+            width={450}
+            height={250}
+            alt="cart"
+          />
         </div>
-      <div>
-        Десерты
-        <Image src="/main_dessert.jpg" width={450} height={250} alt="cart"/>
+      </div>
+      <div className="flex flex-col m-2 hover:bg-orange-100">
+        <h1 className="uppercase font-bold text-2xl text-red-500">Десерты</h1>
+        <div className="overflow-hidden">
+          <Image
+            className="hover:scale-110 transition duration-500 cursor-pointer object-cover "
+            src="/main_dessert.jpg"
+            width={450}
+            height={250}
+            alt="cart"
+          />
         </div>
+      </div>
     </div>
   );
 }
